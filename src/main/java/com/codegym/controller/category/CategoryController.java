4ab6d880ac813f1cc15a7bc9d0dd45ffe873ab45
Toan-246 <n.v.toan687@gmail.com>
2022-04-13 11:32:45 +0700

    crud category

--- src/main/java/com/codegym/controller/category/CategoryController.java
+++ src/main/java/com/codegym/controller/category/CategoryController.java
@@ -5,10 +5,9 @@ import com.codegym.service.category.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -16,9 +15,43 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     ICategoryService categoryService;
+
     @GetMapping
-    public ResponseEntity<Iterable<Category>>findAll (){
+    public ResponseEntity<Iterable<Category>> findAll() {
         Iterable<Category> categories = categoryService.findAll();
         return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Optional<Category> category = categoryService.findById(id);
+        if (!category.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(category.get(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Category>saveCategory (@RequestBody Category category){
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Long id,@RequestBody Category newCategory){
+        Optional<Category> category = categoryService.findById(id);
+        if (!category.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        newCategory.setId(category.get().getId());
+        return new ResponseEntity<>(categoryService.save(newCategory), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory (@PathVariable Long id){
+        Optional<Category> optionalCategory = categoryService.findById(id);
+        if (!optionalCategory.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        categoryService.deleteById(id);
+        return new ResponseEntity<>(optionalCategory.get(), HttpStatus.OK);
     }
 }
