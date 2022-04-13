3c071b5e11a36ff53a61de1d3604fd5dbdd83878
Kiu Kyu <minkiukiu@gmail.com>
2022-04-13 10:58:50 +0700

    sua lai Iterable<T> findAll thanh Page<T> findAll

--- src/main/java/com/codegym/service/IGeneralService.java
+++ src/main/java/com/codegym/service/IGeneralService.java
@@ -2,11 +2,12 @@ package com.codegym.service;
 
 import com.codegym.model.ticket.BorrowTicket;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface IGeneralService<T> {
-    Iterable<T> findAll();
+    Page<T> findAll(Pageable pageable);
 
     T save(T t);
 
