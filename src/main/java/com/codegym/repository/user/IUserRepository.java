2016de7c80fea69e9d8d47a990bfaf9cebefa428
LuytBui <luytbq43@gmail.com>
2022-04-17 10:30:53 +0700

    sua api ban user

--- src/main/java/com/codegym/repository/user/IUserRepository.java
+++ src/main/java/com/codegym/repository/user/IUserRepository.java
@@ -1,6 +1,9 @@
 package com.codegym.repository.user;
 
+import com.codegym.model.user.Role;
 import com.codegym.model.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,5 @@ import java.util.Optional;
 @Repository
 public interface IUserRepository extends PagingAndSortingRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Page<User> findAllByRole_Name(String role_name, Pageable pageable);
 }
